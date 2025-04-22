e0502baf41ac95ee619457b882083fe0e28220c6
Zuari11 <singhuchtam@gmail.com>
2025-04-23 00:41:39 +0530

    init

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /* config options here */
-  images: {
-    domains: ['res.cloudinary.com', 'html.tailus.io'],
-  },
-};
-
-module.exports = nextConfig; 
